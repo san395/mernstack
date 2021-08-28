d1df89da358c474c9e2fe93d4512872731708dd7
sanchit <sanchitjain395@gmail.com>
2021-08-28 19:40:26 +0530

    Adeed a controller and route

---
+++ routes/index.js
@@ -0,0 +1,7 @@
+const express=require('express');
+const router =express.Router();
+const homecontroller =require('../controllers/home_controllers');
+router.get('/',homecontroller.home);
+
+
+module.exports=router;
