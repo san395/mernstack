ec667ed231afd69ec1d07bac09c117e4978228f4
sanchit <sanchitjain395@gmail.com>
2021-08-28 20:19:30 +0530

    all set after 2 controllers added

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 const express=require('express');
 const router =express.Router();
 const homecontroller =require('../controllers/home_controllers');
+router.use('/users',require('./users'));
 router.get('/',homecontroller.home);
 
 
--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,5 @@
 const express=require('express');
-const router=express.router();
+const router=express.Router();
 const users_controllers=require('../controllers/users_controllers');
 router.get('/profile',users_controllers.profile);
 
