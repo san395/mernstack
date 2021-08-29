d8f5faada96d942c04ae88a851aa1c28469e9e00
sanchit <sanchitjain395@gmail.com>
2021-08-29 09:24:06 +0530

    added home view

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const app=express();
 const port=8000;
 // use express router
 app.use('/',require('./routes'));
-
+app.set('view engine','ejs');
+app.set('views','./views')
 app.listen(port,function(err){
       if(err){
           console.log(`Error: ${err}`);
